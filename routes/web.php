f3fba3f7b431c654a566b5222ff557964408ef8e
Ansam_Almgdlawi <ansamalmgdlawi@gmail.com>
2025-12-09 00:46:22 +0300

    db railway

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 use Illuminate\Support\Facades\Mail;
 
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Laravel + Docker is working!',
+        'status' => 'success',
+    ]);
+});
+
 Route::get('/test-mail', function () {
     Mail::raw('Test email works!', function ($message) {
         $message->to('ansamalmgdlawi@gmail.com')
